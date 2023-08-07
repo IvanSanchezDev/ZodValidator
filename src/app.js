45ce7faa2e53c57ec3b0bd0ecb16ce6be92ae8e0
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-07 15:53:28 -0500

    feat: :sparkles: EndPoint de actualizar datos

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { validateMovie } from './schemas/movie.js'
+import { validateMovie, validatePartialMovie } from './schemas/movie.js'
 import crypto from 'node:crypto'
 import { movies } from '../movies.js'
 
@@ -25,6 +25,32 @@ app.post('/movies', (req, res) => {
 
   movies.push(newMovie)
   res.status(201).send(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (result.error) {
+    const errorMessages = result.error.issues.map((error) => error.message)
+    return res.status(400).json({ error: errorMessages })
+  }
+
+  const { id } = req.params
+
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie no found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
 })
 
 const port = process.env.PORT ?? 1234
